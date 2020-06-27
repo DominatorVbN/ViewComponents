aa8ab4bb5c7f5fbc0f33935ad35272bad689bb4d
Dominator <amitsamant@outlook.com>
2020-06-27 18:16:22 +0530

    Initial Commit

---
+++ Shared/ViewComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewComponentsApp.swift
+//  Shared
+//
+//  Created by dominator on 27/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
